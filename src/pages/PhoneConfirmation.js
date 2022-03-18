e2ffc0f1929740c5ed2ee2917f3d3071b2afd226
RukshanNuwan <rukshannuwanbit@gmail.com>
2022-03-18 23:54:49 +0530

    phone confirmation screen Links

--- src/pages/PhoneConfirmation.js
+++ src/pages/PhoneConfirmation.js
@@ -1,5 +1,6 @@
 import {useState} from "react";
 import {Link} from 'react-router-dom';
+import PhoneInput from 'react-phone-number-input';
 import style from '../style/phoneConfirm.module.css';
 
 const PhoneConfirmation = () => {
@@ -7,14 +8,36 @@ const PhoneConfirmation = () => {
 
   return (
     <div className={style.PhoneConfirmContainer}>
+      <Link
+        exact="true"
+        to="/"
+        className={style.BackButton}
+      >
+        <img src="images/arrow.png" alt="back button"/>
+      </Link>
+
       <h1>
         Enter your phone
       </h1>
+
+      <PhoneInput
+        international={true}
+        defaultCountry="US"
+        value={value}
+        onChange={setValue}/>
 
       <p>
         By entering your number, you're agreeing to our <span>Terms of Service and Privacy policy. </span>
         Thanks!
       </p>
+
+      <Link
+        exact="true"
+        to="/code_confirm"
+        className="primaryButton d-flex align-items-center"
+      >
+        Next <img src="images/nextArrowIcon.svg" alt="" className="mx-1"/>
+      </Link>
     </div>
   );
 };
